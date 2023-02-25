65defe47429818f66eef5fa9723108c1674fa842
Velislav Marinov <velislav.marinov.design@gmail.com>
2023-02-25 21:49:57 +0200

    Added some console interactions.

--- src/main/java/qa/automation/MyFirstJava.java
+++ src/main/java/qa/automation/MyFirstJava.java
@@ -1,7 +1,45 @@
 package qa.automation;
 
+import java.util.Scanner;
+
 public class MyFirstJava {
     public static void main(String[] args) {
+        // writes the text in the console
+        System.out.println("Please enter your name!");
+
+        //Scanner is class for reading values from the console
+        Scanner scanner = new Scanner(System.in);
+
+        //a string variable that will store the value entered from the user
+        String firstName = scanner.next();
+
+        //System.out.println("Your name is: " + firstName);
+
+        System.out.println("Please enter your age!");
+
+        //integer variable that will store the age entered from the user
+        int userAge = scanner.nextInt();
+
+
+        System.out.println("Your name is " + firstName +
+                ",\nand you are " + userAge + " years old!");
+
+        /*
+        System.out.println("Your name is: " + firstName);
+        System.out.println("Your age is: " + userAge);
+         */
+
+        if (userAge >= 18){
+            System.out.println(firstName + " can buy alcohol legally!");
+        }
+
+        if (userAge < 18){
+            System.out.println(firstName + " can`t buy alcohol legally!");
+        }
+
+        if (userAge >= 65){
+            System.out.println(firstName + " is retired.");
+        }
 
     }
 }
